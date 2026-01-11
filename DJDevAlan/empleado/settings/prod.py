a3d12c2397630cd2f59bcfe4dfe8039009c1dbe3
Alan Gonzalez <neunapp.goba080428hhgnrla0@soycecytem.mx>
2026-01-11 16:15:02 -0600

    Actu5

--- DJDevAlan/empleado/settings/prod.py
+++ DJDevAlan/empleado/settings/prod.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DJDevAlan.urls'
+ROOT_URLCONF = 'empleado.urls'
 
 TEMPLATES = [
     {
@@ -54,7 +54,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DJDevAlan.wsgi.application'
+WSGI_APPLICATION = 'empleado.wsgi.application'
 
 # DATABASE
 DATABASE_URL = os.environ.get('DATABASE_URL')
