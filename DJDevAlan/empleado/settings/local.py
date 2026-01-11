829900c3db565add192f7a5d5d2d717fd98aafde
Alan Gonzalez <neunapp.goba080428hhgnrla0@soycecytem.mx>
2026-01-11 15:55:44 -0600

    BD

--- DJDevAlan/empleado/settings/local.py
+++ DJDevAlan/empleado/settings/local.py
@@ -12,10 +12,13 @@ ALLOWED_HOSTS = ['mi-sitio-web-de-render.onrender.com']
 #Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+database_url = os.environ.get('DATABASE_URL')
+
+if not database_url:
+    raise ValueError("DATABASE_URL no está configurada")
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.parse(database_url, conn_max_age=600, ssl_require=True)
 }
 
 
